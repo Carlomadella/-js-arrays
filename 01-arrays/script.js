17b77de0f1fccffa7796fc71412d534ceb2c0781
Carlomadella <madella871@gmail.com>
2025-03-19 17:35:28 +0100

    [UPD] 01-arrays completato es. 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,10 +16,12 @@ console.log(fourthTeacher);
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = "Patrick";
 console.log(teachers);
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 
+// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+const firstTeacher = teachers.shift();
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
