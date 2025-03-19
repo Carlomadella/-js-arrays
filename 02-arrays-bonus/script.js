4e3b28a7b0543fa03e19618d1e6d9c70c6359b4e
Carlomadella <madella871@gmail.com>
2025-03-19 17:40:12 +0100

    [UPD] 02-arrays bonus completato es. 4

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -28,7 +28,15 @@ console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 const noEd = teachers.slice("Ed");
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+const isFabioPresent = null;
+for (let i = 0; i < teachers.length; i++) {
+  if (i = "Fabio") {
+    
+  }
+}
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
