734f62c56f2c4934d03fc9afd80cc53debb03eb7
Carlomadella <madella871@gmail.com>
2025-03-19 17:41:01 +0100

    [UPD] 02-arrays bonus completato es. 5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -40,3 +40,13 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+let string = "";
+for (let i = 0; i < teachers.length; i++) {
+string += teachers[i];
+if (i < teachers.length - 1) {
+string += ", ";
+}
+}
+const teachersString = string; 
+
+console.log(teachersString);
